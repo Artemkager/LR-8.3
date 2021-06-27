9c3cb92240cb74f3caea0b827d07d39f2410b747
airex <airex@DESKTOP-PAB22CS>
2021-06-27 20:17:13 +0300

    Добавьте файлы проекта.

---
+++ LR 8.3/LR 8.3.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#define N 5 // Количество чисел
+float average(int arr[]);
+main() {
+    int a[N];
+    int i, min1, min2, buff;
+    srand(time(NULL));
+    for (i = 0; i < N; i++) {
+        a[i] = rand() % 100;// формировка рандомных чисел (для более адекватных значений делю на 100)
+        printf("%3d", a[i]);
+    }
+    printf("\n %.2f\n", average(a));
+}
+float average(int arr[]) {
+    int sum, i;
+    sum = 0;
+    for (i = 0; i < N; i++) sum += arr[i];
+    return sum;
+}
